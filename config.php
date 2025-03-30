95970366c3e68d0cfd8d16524a30bd35dd54dc9e
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-03-30 13:57:51 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/**	op-layout-model:/config.php
+ *
+ * @created     2023-02-06  op-layout-flexbox:/config.php
+ * @copied      2025-03-28  op-layout-flexbox:/config.php --> op-layout-model:/config.php
+ * @version     1.0
+ * @package     op-layout-model
+ * @author      Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright   Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP\LAYOUT\MODEL;
+
+//	...
+return [
+];
