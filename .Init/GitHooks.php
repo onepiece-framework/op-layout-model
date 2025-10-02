da920374caec0925ab0b463b4e141893e485e57f
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-02 18:41:41 +0900

    Add: .Init > GitHooks.php | PHPDoc

---
+++ .Init/GitHooks.php
@@ -0,0 +1,19 @@
+<?php
+/**	op-asset-model:/.Init/GitHooks.php
+ *
+ * @created   2025-10-02
+ * @version   1.0
+ * @package   op-asset-model
+ * @author    Tomoaki Nagahara
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+
+/**	declare
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
