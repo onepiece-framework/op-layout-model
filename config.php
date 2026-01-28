df60aa8f8fa42dbc30bcd02f37445e78177f6bf0
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-28 17:47:37 +0900

    New: config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php
+/**	op-layout-model:/config.php
+ *
+ * @created     2023-02-06  op-layout-flexbox:/config.php
+ * @copied      2025-03-28  op-layout-flexbox:/config.php --> op-layout-model:/config.php
+ * @license     Apache-2.0
+ * @package     op-layout-model
+ * @copyright   Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP\LAYOUT\MODEL;
+
+//	...
+return [
+];
